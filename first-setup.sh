7d649194d138d145f9fa2e87e75aa689acc4bfbd
vmath3us <vmatheusborges@gmail.com>
2023-01-09 08:49:09 -0400

    fixing command to copy hooks, paralleldownload sed

--- first-setup.sh
+++ first-setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
+yaydir=$(mktemp -d -p /tmp)
+sudo pacman -Syu reflector --noconfirm --needed &&
+sudo reflector --save /etc/pacman.d/mirrorlist --country US,UK --protocol https --latest 50 &&
+sudo sed -i "s|ParallelDownloads.*|ParallelDownloads = 15|g" /etc/pacman.conf &&
 sudo pacman -Syu base-devel git --noconfirm --needed &&
-cp export-bin.sh /usr/share/libpalm/scritps/export-bin.sh &&
-cp 99-export-bin.hook /usr/share/libpalm/hooks/99-export-bin.hook &&
-git clone https://aur.archlinux.org/yay-bin /tmp/yay-bin &&
-cd /tmp/yay-bin &&
+sudo cp export-bin.sh /usr/share/libalpm/scripts/export-bin.sh &&
+sudo cp 99-export-bin.hook /usr/share/libalpm/hooks/99-export-bin.hook &&
+git clone https://aur.archlinux.org/yay-bin $yaydir &&
+cd $yaydir &&
 makepkg -s &&
 sudo pacman -U *.zst --noconfirm
