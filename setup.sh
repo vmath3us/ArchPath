27431539e275392f51be58f40ea2e206ade1c987
vmath3us <vmatheusborges@gmail.com>
2023-01-09 07:03:19 -0400

    setup.sh: reorder functions

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env sh
-command_for_setup="sudo touch /etc/subuid /etc/subgid && sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $USER && podman system migrate"
-if command -v podman && command -v sudo && command -v curl; then
-    exec $command_for_setup
-    printf "\no podman está instalado, tentando configurar o modo rootless usando sudo e systemd\n" &&
-    mkdir_setup
-else
-    printf "\ncomandos não encontrados"
-fi
-mkdir_setup{
+command_for_setup(){
+sudo touch /etc/subuid /etc/subgid && sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $USER && podman system migrate
+}
+mkdir_setup(){
 printf "\ncriando diretórios\n"
 mkdir -p $HOME/.local/bin
 mkdir -p $HOME/.local/bin/archpath
@@ -16,7 +11,7 @@ printf "\ncriando arquivo de configuração do distrobox\n"
 printf "container_image="archlinux"\ncontainer_manager="podman"" > $HOME/.config/distrobox/distrobox.conf &&
 distrobox_initial_setup
 }
-distrobox_initial_setup{
+distrobox_initial_setup(){
 printf "\ninstalando Distrobox\n"
 printf "\nhttps://github.com/89luca89/distrobox\n"
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local &&
@@ -29,3 +24,10 @@ printf "\ncontainer criado, preparando para instalar o Yay\n"
 printf "\nhttps://github.com/Jguer/yay\n"
 $HOME/.local/bin/distrobox enter -n ArchPath --  /usr/bin/bash first-setup.sh
 }
+if command -v podman && command -v sudo && command -v curl; then
+     command_for_setup &&
+     printf "\no podman está instalado, tentando configurar o modo rootless usando sudo e systemd\n" &&
+     mkdir_setup
+ else
+     printf "\ncomandos não encontrados"
+ fi
