c8c7958e07a44c4e1bb70a662f187c0de3df8d10
vmath3us <vmatheusborges@gmail.com>
2023-01-11 12:40:07 -0400

    better identation

--- setup.sh
+++ setup.sh
@@ -3,32 +3,51 @@ command_for_setup(){
 sudo touch /etc/subuid /etc/subgid && sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $USER && podman system migrate
 }
 mkdir_setup(){
+printf "###########################################################################"
 printf "\ncriando diretórios\n"
-mkdir -p $HOME/.local/bin
-mkdir -p $HOME/.local/bin/archpath
-mkdir -p $HOME/.config/distrobox
-mkdir -p $HOME/.cache/pacmancache
+mkdir -pv $HOME/.local/bin
+printf "###########################################################################"
+mkdir -pv $HOME/.local/bin/archpath
+printf "###########################################################################"
+mkdir -pv $HOME/.config/distrobox
+mkdir -pv $HOME/.cache/pacmancache
+printf "###########################################################################"
 printf "\ncriando arquivo de configuração do distrobox\n"
+printf "###########################################################################"
 printf "container_image="archlinux"\ncontainer_manager="podman"" > $HOME/.config/distrobox/distrobox.conf &&
+printf "###########################################################################"
 distrobox_initial_setup
 }
 distrobox_initial_setup(){
+printf "###########################################################################"
 printf "\ninstalando Distrobox\n"
+printf "###########################################################################"
 printf "\nhttps://github.com/89luca89/distrobox\n"
+printf "###########################################################################"
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local &&
+printf "###########################################################################"
 first_entry_setup
 }
 first_entry_setup (){
+printf "###########################################################################"
 printf "\n Distrobox instalado, criando container...\n"
 $HOME/.local/bin/distrobox create ArchPath --volume /home/usuario/.cache/pacmancache:/var/cache/pacman/pkg:rw &&
 printf "\ncontainer criado, preparando para instalar o Yay\n"
+printf "###########################################################################"
 printf "\nhttps://github.com/Jguer/yay\n"
-$HOME/.local/bin/distrobox enter -n ArchPath --  /usr/bin/bash first-setup.sh
+$HOME/.local/bin/distrobox enter -n ArchPath --  /usr/bin/bash setup-in-container.sh &&
+printf "###########################################################################"
+printf "\ncole o conteúdo de setup.shell em seu .SHELLrc, e execute exec $SHELL\n"
+printf "###########################################################################"
+printf "\napós instalar algo via yay, basta executar distrobox-import\n"
+printf "###########################################################################%s\n"
 }
+printf "###########################################################################\n                           vmatheus/ArchPath                           \n###########################################################################%s\n"
+
 if command -v podman && command -v sudo && command -v curl; then
-     command_for_setup &&
-     printf "\no podman está instalado, tentando configurar o modo rootless usando sudo\n" &&
-     mkdir_setup
- else
-     printf "\ncomandos não encontrados"
- fi
+    command_for_setup &&
+    printf "\no podman está instalado, tentando configurar o modo rootless usando sudo\n" &&
+    mkdir_setup
+else
+    printf "\ncomandos não encontrados"
+fi
