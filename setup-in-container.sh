4f9f51067981430ca6d798b444c60b45e8da6b44
vmath3us <vmatheusborges@gmail.com>
2023-01-16 08:21:12 -0400

    full automatic using which

--- setup-in-container.sh
+++ setup-in-container.sh
@@ -4,13 +4,14 @@ sudo sed -i "s|ParallelDownloads.*|ParallelDownloads = 15\nDisableDownloadTimeou
 sudo pacman -Fyyy &&
 sudo pacman -Syu reflector --noconfirm --needed &&
 sudo reflector --save /etc/pacman.d/mirrorlist --country US,UK --protocol https --latest 50
-sudo pacman -Syu neovim base-devel git --noconfirm --needed &&
-sudo cp export-bin.sh /usr/share/libalpm/scripts/export-bin.sh &&
-sudo cp 99-export-bin.hook /usr/share/libalpm/hooks/99-export-bin.hook &&
-sudo cp distrobox-import-handler /usr/bin/distrobox-import-handler &&
+sudo pacman -Syu base-devel git --noconfirm --needed &&
+sudo cp distrobox-import-handler /usr/bin/distrobox-import &&
+sudo cp distrobox-archpath-save /usr/bin/distrobox-archpath-save &&
 git clone https://aur.archlinux.org/yay-bin $yaydir &&
 cd $yaydir &&
 makepkg -s &&
 sudo pacman -U *.zst --noconfirm
 distrobox-export --bin /usr/sbin/yay --export-path $HOME/.local/bin/archpath
 distrobox-export --bin /usr/sbin/pacman --export-path $HOME/.local/bin/archpath
+distrobox-export --bin /usr/sbin/distrobox-import --export-path $HOME/.local/bin/archpath
+distrobox-export --bin /usr/sbin/distrobox-archpath-save --export-path $HOME/.local/bin/archpath
